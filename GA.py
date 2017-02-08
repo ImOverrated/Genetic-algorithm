4b99e336031c3777e7a85fa14f073f1647d0ca5f
ImOverrated <collot.julien5@gmail.com>
2017-02-08 21:55:30 +0100

    Update GA.py

--- GA.py
+++ GA.py
@@ -1,35 +1,36 @@
 import string
 import random
 import math
+import pygal
 
 EXPECTED_STRING = "Hello world"
 
 POPULATION_SIZE = 100
 
-GENERATION_COUNT_MAX = 10000
+GENERATION_COUNT_MAX = 10000000
 
 CHANCE_TO_MUTATE = 0.1
 GRADED_RETAIN_PERCENT = 0.2
 CHANCE_RETAIN_NONGRATED = 0.05
 
-GRADED_INDIVIDUAL_RETAIN_COUNT = (POPULATION_SIZE * GRADED_RETAIN_PERCENT)
+GRADED_INDIVIDUAL_RETAIN_COUNT = int(POPULATION_SIZE * GRADED_RETAIN_PERCENT)
 
 MAX_SCORE = len(EXPECTED_STRING)
 MIDDLE_LENGTH = len(EXPECTED_STRING) / 2
 if not MIDDLE_LENGTH.is_integer:
-	LEFT_LENGTH = round(MIDDLE_LENGTH)
-	RIGHT_LENGTH = math.floor(MIDDLE_LENGTH)
+	LEFT_LENGTH = int(round(MIDDLE_LENGTH))
+	RIGHT_LENGTH = int(math.floor(MIDDLE_LENGTH))
 else:
-	LEFT_LENGTH = MIDDLE_LENGTH
-	RIGHT_LENGTH = MIDDLE_LENGTH
+	LEFT_LENGTH = int(MIDDLE_LENGTH)
+	RIGHT_LENGTH = int(MIDDLE_LENGTH)
 
 def get_random_char():
     return random.choice(string.ascii_letters + ' ')
 
 def generate_individual():
-	individual = ""
+	individual = []
 	for _ in range(len(EXPECTED_STRING)):
-		individual += get_random_char()
+		individual.append(get_random_char())
 	return individual
 
 def generate_population():
@@ -55,27 +56,29 @@ def order_population_by_score(population):
 	return sorted(order_population, key=lambda x: x[1], reverse=True)
 
 def evol_population(population):
-	ordered_population = order_population_by_score(population)
+	raw_order_population = order_population_by_score(population)
 	solution = []
+	order_population = []
 
-	for individual, score in ordered_population:
+	for individual, score in raw_order_population:
+		order_population.append(individual)
 		if(score == MAX_SCORE):
 			solution.append(individual)
 
 	if solution:
 		return population, average_population_score(population), solution
 
-	parents = ordered_population[:GRADED_INDIVIDUAL_RETAIN_COUNT]
+	parents = order_population[:GRADED_INDIVIDUAL_RETAIN_COUNT]
 
 	# Promote random individual for diversity
 	for individual in order_population[GRADED_INDIVIDUAL_RETAIN_COUNT:]:
-		if(random() < CHANCE_RETAIN_NONGRATED):
+		if(random.random() < CHANCE_RETAIN_NONGRATED):
 			parents.append(individual)
 
 	# Random mutation
 	for individual in parents:
-		if(random() < CHANCE_TO_MUTATE):
-			place_to_modify = random.randint(0, MAX_SCORE)
+		if(random.random() < CHANCE_TO_MUTATE):
+			place_to_modify = random.randint(0, MAX_SCORE - 1)
 			individual[place_to_modify] = get_random_char()
 
     # Crossover parent to create child
@@ -83,6 +86,7 @@ def evol_population(population):
 	desired_len = POPULATION_SIZE - parents_len
 	children = []
 	while(len(children) != desired_len):
+		#print(str(len(children)) + ' //// ' + str(desired_len))
 		father = random.choice(parents)
 		mother = random.choice(parents)
 
@@ -90,7 +94,7 @@ def evol_population(population):
 		children.append(child)
 
 	parents.extend(children)
-	return parents, solution
+	return parents, average_population_score(population), solution
 
 
 def main():
@@ -105,10 +109,11 @@ def main():
 	log_avg = []
 	while not solution and i < GENERATION_COUNT_MAX:
 		population, average_score, solution = evol_population(population)
-		if i == 255:
-			print("Current score : " +average_score)
-		if i == 31:
+		if i & 255 == 255:
+			print("Current score : " + str(average_score))
+		if i & 31 == 31:
 			log_avg.append(average_score)
+		i+=1
 
 	# Generation de l'évolution du score de la population
 	line_chart = pygal.Line(show_dots=False, show_legend=False)
@@ -119,7 +124,7 @@ def main():
 	line_chart.render_to_file("bar_chart.svg")
 
 	final_average_score = average_population_score(population)
-	print("Final Score : " + final_average_scores)
+	print("Final Score : " + str(final_average_score))
 
 	if solution:
 		print("Solution found (%d times) after %d generations." % (len(solution), i))
@@ -128,8 +133,6 @@ def main():
 		print("- Last population was:")
 		for number, individual in enumerate(population):
 			print(number, '->',  ''.join(individual))
-
-	#print(order_population_by_score(generate_population()))
 
 main()
 			
